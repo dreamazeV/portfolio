3e29f5e895e0fee360c31dc8ef54b5444ee298c5
dreamazeV <dreamazeV@users.noreply.github.com>
2025-08-11 20:27:18 +0000

    deploy: d0dec95e9a239cca6145ec88ba3599a74030c2ac

---
+++ precache-manifest.363e67082bf72435aa0bbb351252ec67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb5e5b08225ae9edf2bffbd3d96903a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba2231888b5af10d8036",
+    "url": "./static/css/2.02e251ca.chunk.css"
+  },
+  {
+    "revision": "77fc01ef1302110a955b",
+    "url": "./static/css/main.d184d41b.chunk.css"
+  },
+  {
+    "revision": "ba2231888b5af10d8036",
+    "url": "./static/js/2.938a6a6d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.938a6a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fc01ef1302110a955b",
+    "url": "./static/js/main.9212590f.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
